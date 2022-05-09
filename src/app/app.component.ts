2be6da952f2ec2a07ed860e44494ebe9739dd464
Erinxon <30541728+Erinxon@users.noreply.github.com>
2022-05-09 16:28:20 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string = 'Erinxon Ariel Santana Santana';
+  bio: string = 'Mi nombre es Erinxon Santana, tengo 22 años, egresado del ITLA de la carrera de desarrollo de Software y actualmente me desempeño como Técnico Programador en el ITLA';
+}
